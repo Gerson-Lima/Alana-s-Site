0643f316c48823a5d1ad4d330db07f27087c863a
Gerson-Lima <gersondouglas2011@gmail.com>
2025-07-15 14:14:29 -0300

    feat: preloader img

--- src/Components/Organisms/Hero/Hero.jsx
+++ src/Components/Organisms/Hero/Hero.jsx
@@ -1,54 +1,64 @@
-// import React from "react";
+import React, { useEffect } from "react";
 import imgHero from "../../../assets/img-hero.webp";
 import "./Hero.css";
 
 export function Hero() {
+
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.rel = 'preload';
+    link.as = 'image';
+    link.href = imgHero;
+    document.head.appendChild(link);
+
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, []);
+
   return (
     <section className="flex flex-col lg:flex-row items-center justify-center px-4 pt-20 lg:pt-32 sm:pt-32 hero-bg" id="home">
-      {/* Container para alinhar com o header */}
-        <div className="mx-auto flex flex-col lg:flex-row items-center justify-between space-y-6 lg:space-y-0 lg:space-x-8 w-full max-w-7xl px-0 sm:px-7 xl:px-0">
-            {/* Texto à esquerda */}
-            <div className="text-left max-w-xl pb-16">
-            <h1 className="lg:text-4xl sm:text-[18px] md:text-[40px] 2sm:text-[34px] font-primary font-extrabold text-gray-800 mb-3 flex flex-col">
-                <span className="text-[#202020] uppercase">
-                Transformando sonhos
-                </span>
-                <span className="text-secondary uppercase text-right xl:ml-52 mt-2">
-                em espaços únicos
-                </span>
-            </h1>
+      <div className="mx-auto flex flex-col lg:flex-row items-center justify-between space-y-6 lg:space-y-0 lg:space-x-8 w-full max-w-7xl px-0 sm:px-7 xl:px-0">
+        <div className="text-left max-w-xl pb-16">
+          <h1 className="lg:text-4xl sm:text-[18px] md:text-[40px] 2sm:text-[34px] font-primary font-extrabold text-gray-800 mb-3 flex flex-col">
+            <span className="text-[#202020] uppercase">
+              Transformando sonhos
+            </span>
+            <span className="text-secondary uppercase text-right xl:ml-52 mt-2">
+              em espaços únicos
+            </span>
+          </h1>
 
-            <div className="lg:w-96 sm:w-52 h-[1.5px] bg-[#202020] mt-4 sm:mt-2"></div>
+          <div className="lg:w-96 sm:w-52 h-[1.5px] bg-[#202020] mt-4 sm:mt-2"></div>
 
-            <p className="xl:text-[24px] sm:text-[16px] text-[#202020] font-primary mb-6 mt-2 leading-tight">
-                Entre em contato e vamos dar vida
-                <br className="hidden md:block" /> 
-                ao seu novo ambiente!
-            </p>
-            <a
-                href="#contact"
-                className="inline-block px-6 py-3 bg-secondary text-white font-semibold font-primary rounded-[2px] hover:bg-[#8E5858] transition"
+          <p className="xl:text-[24px] sm:text-[16px] text-[#202020] font-primary mb-6 mt-2 leading-tight">
+            Entre em contato e vamos dar vida
+            <br className="hidden md:block" /> 
+            ao seu novo ambiente!
+          </p>
+          <a
+            href="#contact"
+            className="inline-block px-6 py-3 bg-secondary text-white font-semibold font-primary rounded-[2px] hover:bg-[#8E5858] transition"
+          >
+            Tire suas dúvidas{" "}
+            <span
+              className="pl-2"
+              style={{ position: "relative", top: "2px" }}
             >
-                Tire suas dúvidas{" "}
-                <span
-                className="pl-2"
-                style={{ position: "relative", top: "2px" }}
-                >
-                {" "}
-                &gt;{" "}
-                </span>
-            </a>
-            </div>
-
-            {/* Imagem à direita */}
-            <div className="lg:block w-full lg:w-auto">
-            <img
-                src={imgHero}
-                alt="Imagem hero"
-                className="w-auto lg:max-w-lg object-cover"
-            />
-            </div>
+              {" "}
+              &gt;{" "}
+            </span>
+          </a>
         </div>
+
+        <div className="lg:block w-full lg:w-auto">
+          <img
+            src={imgHero}
+            alt="Imagem hero"
+            className="w-auto lg:max-w-lg object-cover"
+          />
+        </div>
+      </div>
     </section>
   );
 }
